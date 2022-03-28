a126b2044534ee8198336715b80e5a08cd185c9c
Hurricane <87663837+Ethanjp2008@users.noreply.github.com>
2022-03-28 13:03:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+noseX = 0;
+noseY = 0;
+
+
 function preload(){
+    mustache_image = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup(){
@@ -14,6 +19,7 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(mustache_image, noseX-15, noseY, 30, 30);
 }
 
 function take_snapshot(){
@@ -27,7 +33,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        console.log("nose x: " + results[0].pose.nose.x);
-        console.log("nose y: " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x: " + noseX);
+        console.log("nose y: " + noseY);
     }
 }
